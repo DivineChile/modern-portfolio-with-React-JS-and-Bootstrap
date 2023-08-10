5a52440952676729cbd7e098920ef665ece72c46
Divine Chilee <divinechile16@gmail.comm>
2023-08-10 15:23:08 +0100

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React from "react";
-
-import {
-  Footer,
-  Blog,
-  Possibility,
-  Features,
-  WhatGPT3,
-  Header,
-} from "./containers";
-import { CTA, Brand, Navbar } from "./components";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+    <div>
+      <h2>Portfolio Project</h2>
     </div>
   );
 };
